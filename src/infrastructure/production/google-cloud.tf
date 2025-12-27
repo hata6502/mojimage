810248ab974819930e577d6de55f9be3616bc780
hata6502 <7702653+hata6502@users.noreply.github.com>
2025-12-28 02:36:37 +0900

    .

--- src/infrastructure/production/google-cloud.tf
+++ src/infrastructure/production/google-cloud.tf
@@ -99,3 +99,21 @@ resource "google_cloud_run_domain_mapping" "server" {
     route_name = google_cloud_run_v2_service.server.name
   }
 }
+
+resource "google_storage_bucket" "image" {
+  name                        = "image-${var.google_cloud_project}"
+  location                    = var.google_cloud_region
+  uniform_bucket_level_access = true
+
+  cors {
+    origin          = ["https://mojimage.hata6502.com"]
+    method          = ["GET", "HEAD"]
+    response_header = ["range", "etag", "if-match"]
+    max_age_seconds = 300
+  }
+}
+resource "google_storage_bucket_iam_member" "image_object_viewer" {
+  bucket = google_storage_bucket.image.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
